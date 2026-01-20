394761d93af45c89f48c3dbeb9257c64c1455149
jnmry <150659815+jnmry@users.noreply.github.com>
2026-01-20 23:33:28 +0800

    Add files via upload

---
+++ SteelDetection/Config.py
@@ -0,0 +1,11 @@
+#coding:utf-8
+
+# 图片及视频检测结果保存路径
+save_path = 'save_data'
+
+# 使用的模型路径
+model_path = 'models/best.pt'
+
+names = {0: 'crazing',1:"inclusion",2:"patches",3:"pitted_surface",4:"rolled_in_scale",5:"scratches"}
+
+CH_names = ['划痕','夹杂物','补丁','凹痕表面','卷曲尺寸','划痕']
